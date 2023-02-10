5bc37e5f450cdf2f353be7f43244195d410c6c51
German Gallardo <ggallardoh.90@gmail.com>
2023-02-10 06:56:30 -0300

    Added overlapping methods to model

--- app/models/route.rb
+++ app/models/route.rb
@@ -16,13 +16,19 @@ class Route < ApplicationRecord
 
     event :schedule do
       transitions from: :unscheduled, to: :scheduled,
-                  guard: :no_overlapping_routes?
+                  guard: :overlapping_routes?
+      success do |driver, vehicle|
+        self.driver = driver
+        self.vehicle = vehicle
+        save!
+      end
     end
     event :unschedule do
       transitions from: :scheduled, to: :unscheduled
       success do
         self.driver = nil
         self.vehicle = nil
+        save!
       end
     end
   end
@@ -35,17 +41,28 @@ class Route < ApplicationRecord
     driver.nil? ? 'Sin Asignar' : "#{driver.name}/#{vehicle.plate}"
   end
 
-  private
-
-  def overlapping_routes?
-    routes = []
-    routes << Route.scheduled.where.not(id:).with_driver(driver) if driver.present?
-    routes << Route.scheduled.where.not(id:).with_vehicle(vehicle) if vehicle.present?
+  def overlapping_routes(driver, vehicle)
+    other_routes = []
+    Route.scheduled.where.not(id:).with_driver(driver).to_a.each { |r| other_routes << r }
+    Route.scheduled.where.not(id:).with_vehicle(vehicle).to_a.each { |r| other_routes << r }
     period = Range.new starts_at, ends_at
 
-    routes.none? do |route|
-      route_period = Range.new route.starts_at, route.ends_at
+    other_routes.select! do |other_route|
+      route_period = Range.new other_route.starts_at, other_route.ends_at
       period.overlaps?(route_period)
     end
+    other_routes.uniq
+  end
+
+  def overlapping_routes_message(driver, vehicle)
+    msg = 'Vehiculo o conductor ya asignado a rutas:'
+    overlapping_routes(driver, vehicle).each { |r| msg += " #{r.name}" }
+    msg
+  end
+
+  private
+
+  def overlapping_routes?(driver, vehicle)
+    overlapping_routes(driver, vehicle).empty?
   end
 end
