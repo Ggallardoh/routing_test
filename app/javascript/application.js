78428d6c8b5d5ceac210bd28c1cd0231d9b6dc42
German Gallardo <ggallardoh.90@gmail.com>
2023-02-10 07:02:01 -0300

    Add various dependencies

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,16 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
 import "./controllers"
 import * as bootstrap from "bootstrap"
 //= require underscore
 //= require gmaps/google
+//= require jquery
+//= require jquery-ui
+//= require jquery_ujs
+//= require bootstrap-dropdown
+//= require bootstrap
+//= require bootstrap-sprockets
+//= require turbolinks
+//= require popper
+//= require_tree.
